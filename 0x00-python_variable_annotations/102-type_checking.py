7d1b035cdbd87ff3017c4352fede475e3b01588a
MachariaP <walburphinehas78@gmail.com>
2024-07-05 03:28:55 +0300

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,35 +1,27 @@
 #!/usr/bin/env python3
-
-"""Module that contains a type-annotated function zoom_array
-that takes a tuple of integers and returns a tuple of integers
-with a factor of 2 or 3.0.
-The type of the input tuple is Tuple and the type of
-the output tuple is Tuple
+"""
+Demonstrates type annotations by repeating elements in a tuple.
 """
 
-
-# import Tuple class from the typing module
-from typing import Tuple
+from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    """ Change the annotation for 1st and return the type
-        to Tuple
-        to indicate that the function accepts and
-        returns a tuple of any type.
-        Also, change type of array to tuple() instead of list[].
-        Lastly, change the float value to interger
-        when calling zoom_array
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
-    zoomed_in: Tuple = tuple(
-        item for item in lst
-        for i in range(factor)
-    )
-    return zoomed_in
+    Repeats each tuple element a specified number of times.
+
+    Parameters:
+    - lst: Tuple of integers.
+    - factor: Repetition factor.
+
+    Returns:
+    - List of repeated elements.
+    """
+    return [item for item in lst for _ in range(factor)]
 
 
-array = (12, 72, 91)  # use tuple instead of list
+array = [12, 72, 91]
 
-zoom_2x = zoom_array(array)
+zoom_2x = zoom_array(tuple(array))
 
-zoom_3x = zoom_array(array, 3)  # use interger value for the factor
+zoom_3x = zoom_array(tuple(array), 3)
