81f8723277bb36d21b277a70959636590a10a0d5
Phinehas <walburphinehas78@gmail.com>
2025-11-18 11:55:11 +0300

    install and configure django-filter for message filtering

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # 3rd party
     'rest_framework',
+    'django_filters',
     'rest_framework_simplejwt',
 
     # Local apps
@@ -129,6 +130,7 @@ REST_FRAMEWORK = {
         'DEFAULT_PERMISSION_CLASSES': [
             'rest_framework.permissions.IsAuthenticated',
             ],
+        'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
         'PAGE_SIZE':20
         }
