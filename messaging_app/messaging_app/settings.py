a1ed39e9f5876c0f9b8c51a808233086cd30bc8d
Phinehas <walburphinehas78@gmail.com>
2026-01-21 14:34:18 +0300

    Resolved a syntax error

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -146,7 +146,7 @@ SPECTACULAR_SETTINGS = {
     'TITLE': 'ALX Messaging App API',
     'DESCRIPTION': 'Backend API for real-time messaging application',
     'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': True.
+    'SERVE_INCLUDE_SCHEMA': True,
     'SWAGGER_UI_SETTINGS': {
         'persistAuthorization': True,  # Keeps token after refresh
     }
